9e4f0dc66b5400668b476629635b21b2286fa7aa
Christian Nick Rasmussen <cnr@relateit.dk>
2021-01-06 14:41:49 +0100

    Secont commit

--- main.cpp
+++ main.cpp
@@ -1,44 +1,87 @@
 #include "mbed.h"
 #include "stm32746g_discovery_lcd.h"
+#include <cstdio>
 #define LCD_SMALL_FONT        Font16 //Small font constant
+#define LCD_MEDIUM_FONT        Font20 //Small font constant
 // main() runs in its own thread in the OS
+
+AnalogIn ain(A0);
+DigitalOut myLedD2(D2);
+DigitalOut myLedD3(D3);
+
+uint8_t percentString[25] = {0};
+uint8_t counterString[15] = {0};
+
+int counter = 1;
+
+
+void startScreen() {
+    BSP_LCD_Clear(LCD_COLOR_DARKCYAN);
+    BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+    BSP_LCD_SetBackColor(LCD_COLOR_DARKCYAN);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    BSP_LCD_DisplayStringAt(0, 100, (uint8_t *)"ONSDAGS PROJEKT", CENTER_MODE);
+    BSP_LCD_DrawEllipse(233, 110, 170, 65);
+    BSP_LCD_SetFont(&LCD_SMALL_FONT);
+    BSP_LCD_DisplayStringAt(0, 250, (uint8_t *)"Version 0.0.1", CENTER_MODE);
+    HAL_Delay(2000);
+
+}
+
+void homeScreen() {
+    BSP_LCD_Clear(LCD_COLOR_BLACK);
+    BSP_LCD_SetFont(&LCD_MEDIUM_FONT);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    BSP_LCD_DisplayStringAt(0, 5, (uint8_t *)"ONSDAGS PROJEKT", CENTER_MODE);
+    //BSP_LCD_DisplayStringAt(0, 100, (uint8_t *)percentString, CENTER_MODE);
+}
+
+void updatePercentString() {
+    BSP_LCD_SetFont(&LCD_MEDIUM_FONT);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    BSP_LCD_DisplayStringAt(0, 100, (uint8_t *)percentString, CENTER_MODE);
+}
+
+void updateCounter() {
+    BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    BSP_LCD_DisplayStringAt(0, 130, (uint8_t *)counterString, CENTER_MODE);
+
+}
+
+
 int main()
 {
     BSP_LCD_Init();
     BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
     BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+
+    startScreen();
+    homeScreen();
     while (true) {
-        BSP_LCD_Clear(LCD_COLOR_BLACK);
-        BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
-        BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
-        BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-        BSP_LCD_DisplayStringAt(0, 100, (uint8_t *)"ONSDAGS PROJEKT", CENTER_MODE);
-        BSP_LCD_DrawEllipse(233, 110, 170, 65);
-        BSP_LCD_SetFont(&LCD_SMALL_FONT);
-        BSP_LCD_DisplayStringAt(0, 250, (uint8_t *)"Version 0.0.1", CENTER_MODE);
-        HAL_Delay(2000);
-/*
-        BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
-        BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-        BSP_LCD_DisplayStringAt(0, 100, (uint8_t *)"DISCOVERY STM32F746NG", CENTER_MODE);
-        HAL_Delay(2000);
+        printf("percentage: %3.3f%%\n", ain.read() * 100.0f);
+        printf("Counter: %4d\n",counter);
+        sprintf((char *) percentString, "Power: %3.0f%%", ain.read() * 100.0f);
+        sprintf((char *) counterString,"Counter: %4d",counter);
+        updatePercentString();
+        updateCounter();
+        if(counter < 9999) {
+            counter++;
+        } else {
+            counter = 1;
+        }
 
-        BSP_LCD_Clear(LCD_COLOR_GREEN);
-        BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
-        BSP_LCD_DrawRect(10, 20, 50, 50);
-        BSP_LCD_SetTextColor(LCD_COLOR_BROWN);
-        BSP_LCD_DrawCircle(80, 80, 50);
-        BSP_LCD_SetTextColor(LCD_COLOR_YELLOW);
-        BSP_LCD_DrawEllipse(150, 150, 50, 100);
-        BSP_LCD_SetTextColor(LCD_COLOR_RED);
-        BSP_LCD_FillCircle(200, 200, 40);
-        HAL_Delay(2000);
-
-        BSP_LCD_SetBackColor(LCD_COLOR_GREEN);
-        BSP_LCD_SetTextColor(LCD_COLOR_CYAN);
-        BSP_LCD_DisplayStringAt(0, 6, (uint8_t *)"HAVE FUN !!!", RIGHT_MODE);
-        HAL_Delay(2000);
-        */
+        if (ain > 0.9f) {
+            myLedD3 = !myLedD3;
+            myLedD2 = !myLedD3;
+        } else {
+            myLedD3 = 0;
+            myLedD2 = 0;
+        }
+        wait_us(250000);
     }
 }
 
